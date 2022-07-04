8c9396282b4318e0e9acd208e90ee441c6a6c906
figlerr <figler.renata@tmx.hu>
2022-07-04 17:10:42 +0200

    forras

--- hibernate_with_mysql/src/test/java/hu/hibernatewithmysql/SpringBootJpaMysqlTest.java
+++ hibernate_with_mysql/src/test/java/hu/hibernatewithmysql/SpringBootJpaMysqlTest.java
@@ -6,7 +6,6 @@ import hu.hibernatewithmysql.repository.BookRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ActiveProfiles;
