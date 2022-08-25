fcfae401155d56a7b98a12bc49450ec441d35096
figlerr <figler.renata@tmx.hu>
2022-08-25 10:18:52 +0200

    forras

---
+++ hibernate_primary_keys/src/main/java/hu/hibernate_primary_keys/HibernatePrimaryKeysApplication.java
@@ -0,0 +1,13 @@
+package hu.hibernate_primary_keys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernatePrimaryKeysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernatePrimaryKeysApplication.class, args);
+    }
+
+}
---
+++ hibernate_primary_keys/src/test/java/hu/hibernate_primary_keys/HibernatePrimaryKeysApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.hibernate_primary_keys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernatePrimaryKeysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
